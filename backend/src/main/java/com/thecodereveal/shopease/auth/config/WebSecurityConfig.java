c9f3c827e6d9a4dd9d804a5502a82e11dcd22957
shivu749 <tshivansh2406@gmail.com>
2026-08-30 17:13:35 +0530

    resolving oauth error

--- backend/src/main/java/com/thecodereveal/shopease/auth/config/WebSecurityConfig.java
+++ backend/src/main/java/com/thecodereveal/shopease/auth/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.GET,"/api/products","/api/category").permitAll()
                         .requestMatchers("/oauth2/success").permitAll()
                 .anyRequest().authenticated())
-                .oauth2Login((oauth2login)-> oauth2login.defaultSuccessUrl("/oauth2/success").loginPage("/oauth2/authorization/google"))
+                //.oauth2Login((oauth2login)-> oauth2login.defaultSuccessUrl("/oauth2/success").loginPage("/oauth2/authorization/google"))
                 //.exceptionHandling((exception)-> exception.authenticationEntryPoint(new RESTAuthenticationEntryPoint()))
                 .addFilterBefore(new JWTAuthenticationFilter(jwtTokenHelper,userDetailsService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
